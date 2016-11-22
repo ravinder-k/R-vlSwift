0400c2f1d6fcbd374a0ce4f316d1362467f8f7f7
Apple <apple@Apples-Mac-mini.local>
2016-11-22 18:52:41 +0530

    day update

---
+++ iStorm/View/TableViewCell/PropertyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PropertyTableViewCell.swift
+//  iStorm
+//
+//  Created by Apple on 11/22/16.
+//  Copyright © 2016 Incresol. All rights reserved.
+//
+
+import UIKit
+
+class PropertyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

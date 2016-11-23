4153ab79b1ee1a02a671a1bead64c23d9b996e88
Apple <apple@Apples-Mac-mini.local>
2016-11-23 19:31:19 +0530

    day update 2311

---
+++ iStorm/View/TableViewCell/OptionCell.swift
@@ -0,0 +1,24 @@
+//
+//  OptionCell.swift
+//  iStorm
+//
+//  Created by Apple on 11/23/16.
+//  Copyright © 2016 Incresol. All rights reserved.
+//
+
+import UIKit
+
+class OptionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
